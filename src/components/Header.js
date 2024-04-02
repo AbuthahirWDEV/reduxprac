6f4649eed525ab64a3169f377ad4d082dd299a43
Abu <abu.dev.45@gmail.com>
2024-04-02 06:39:59 +0530

    navigate added to logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
+
   return (
     <div className="ui fixed menu">
-      <div className="ui container center">
-        <h2>FakeShop</h2>
+      <div className="ui container center" onClick={handleClick}>
+        <h2 style={{ cursor: "pointer" }}>FakeShop</h2>
       </div>
     </div>
   );
